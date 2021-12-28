390eafbe192cc493b9819d5b4aecbf34f89ea62e
chenshiwei <chenshiwei@111.com>
2021-12-28 23:36:35 +0800

    1111

---
+++ src/main/java/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: Swing
+ * @Date: 2021/12/28/23:28
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("git测试");
+    }
+}
